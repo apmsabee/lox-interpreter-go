76345a3915ca86ae45b2269eb38c196e34c0815f
Andy Sabee <asabee@rwbaird.com>
2024-08-12 13:07:46 -0500

    Created scanner class to abstract token reading, added processing of equals and equals-equals

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -3,62 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
+
+type Config struct {
+	command  string
+	filename string
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
-	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
-		os.Exit(1)
-	}
+	config := getConfig()
 
-	command := os.Args[1]
+	scanner := newScanner(config.filename)
 
-	if command != "tokenize" {
-		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
-		os.Exit(1)
-	}
-
-	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-		os.Exit(1)
-	}
-
-	tokens := map[rune]string{
-		'(': "LEFT_PAREN ( null",
-		')': "RIGHT_PAREN ) null",
-		'{': "LEFT_BRACE { null",
-		'}': "RIGHT_BRACE } null",
-		'*': "STAR * null",
-		'.': "DOT . null",
-		',': "COMMA , null",
-		'+': "PLUS + null",
-		'-': "MINUS - null",
-		';': "SEMICOLON ; null",
-	}
-
-	convertedContents := (string)(fileContents)
 	cleanRun := true
 
-	if len(convertedContents) > 0 {
-		for index, char := range convertedContents {
-			if scanned, validFile := tokens[char]; validFile {
-				fmt.Println(scanned)
-			} else {
-				line := strings.Count(convertedContents[0:index], "\n") + 1
-				fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %c\n", line, char)
-				cleanRun = false
-			}
-
+	for scanner.current <= len(scanner.fileContents) {
+		if token, errMsg := scanner.nextToken(); errMsg == "" {
+			fmt.Println(token)
+		} else {
+			fmt.Fprint(os.Stderr, errMsg)
+			cleanRun = false
 		}
-		fmt.Println("EOF  null")
-	} else {
-		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
 
 	if cleanRun {
@@ -66,4 +34,23 @@ func main() {
 	} else {
 		os.Exit(65)
 	}
+}
+
+func getConfig() (config Config) {
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+		os.Exit(1)
+	}
+
+	config.command = os.Args[1]
+
+	if config.command != "tokenize" {
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", config.command)
+		os.Exit(1)
+	}
+
+	config.filename = os.Args[2]
+	return
+
 }
---
+++ cmd/myinterpreter/scanner.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Scanner struct {
+	fileContents []byte
+	current      int
+	currentLine  int
+
+	exitCode int
+}
+
+func newScanner(filename string) *Scanner {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	return &Scanner{fileContents: file, currentLine: 1}
+}
+
+func (s Scanner) nextToken() (*Token, string) {
+	if s.current >= len(s.fileContents) {
+		return newToken(EOF, "", nil), ""
+	}
+	currToken := s.fileContents[s.current]
+	s.current++
+	switch currToken {
+	case '(':
+		return newToken(LEFT_PAREN, "(", nil), ""
+	case ')':
+		return newToken(RIGHT_PAREN, ")", nil), ""
+	case '{':
+		return newToken(LEFT_BRACE, "{", nil), ""
+	case '}':
+		return newToken(RIGHT_BRACE, "]", nil), ""
+	case ',':
+		return newToken(COMMA, ",", nil), ""
+	case '.':
+		return newToken(DOT, ".", nil), ""
+	case '+':
+		return newToken(PLUS, "+", nil), ""
+	case '-':
+		return newToken(MINUS, "-", nil), ""
+	case ';':
+		return newToken(SEMICOLON, ";", nil), ""
+	case '*':
+		return newToken(STAR, "*", nil), ""
+	case '\n':
+		s.currentLine++
+		return newToken(NEWLINE, "\\n", nil), ""
+	case '=':
+		if s.current <= len(s.fileContents) && s.fileContents[s.current] == '=' {
+			s.current++
+			return newToken(EQUAL_EQUAL, "==", nil), ""
+		}
+		return newToken(EQUAL, "=", nil), ""
+	default:
+		err := fmt.Sprintf("[line %d] Error: Unexpected character: %c\n", s.currentLine, currToken)
+		return nil, err
+	}
+}
+
+type TokenType int
+
+const (
+	EOF TokenType = iota
+	LEFT_PAREN
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	PLUS
+	MINUS
+	SEMICOLON
+	STAR
+	NEWLINE
+	EQUAL
+	EQUAL_EQUAL
+)
+
+type Token struct {
+	Type    TokenType
+	lexeme  string
+	literal interface{}
+}
+
+func newToken(t TokenType, lexeme string, literal interface{}) *Token {
+	return &Token{Type: t, lexeme: lexeme, literal: literal}
+}
+
+func (t TokenType) String() string {
+	return [...]string{"EOF", "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE", "RIGHT_BRACE", "COMMA", "DOT", "PLUS", "MINUS", "SEMICOLON", "STAR", "NEWLINE", "EQUAL", "EQUAL_EQUAL"}[t]
+}
+
+func (t *Token) String() string {
+	s := fmt.Sprintf("%s %s ", t.Type, t.lexeme)
+	if t.literal != nil {
+		s += fmt.Sprintf("%v", t.literal)
+	} else {
+		s += "null"
+	}
+	return s
+}
